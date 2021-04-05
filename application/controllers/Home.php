b4c33488362baa9af212bbfea9b2cb2cf4349edd
aliwafa16 <aliwafamuhamad16@gmail.com>
2021-04-05 22:27:25 +0700

    second commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,8 @@ class Home extends CI_Controller
 {
     public function index()
     {
-        echo 'Hello World';
+        $this->load->view('user/templates/user_header.php');
+        $this->load->view('user/home/index.php');
+        $this->load->view('user/templates/user_footer.php');
     }
 }
